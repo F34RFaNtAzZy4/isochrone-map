610d575c0d8be16fa15b01597de6bfc96c1fdfbb
Jonas Both <32544262+F34RFaNtAzZy4@users.noreply.github.com>
2025-06-28 19:22:59 +0200

    added suspense for params

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 import IsochroneMapTemplate from "@/components/templates/isochrone-map-template";
 import "leaflet/dist/leaflet.css";
+import { Suspense } from "react";
 
 export default function IsochroneMapPage() {
-  return <IsochroneMapTemplate />;
+  return (
+    <Suspense fallback={<div>Loading params…</div>}>
+      <IsochroneMapTemplate />
+    </Suspense>
+  );
 }
